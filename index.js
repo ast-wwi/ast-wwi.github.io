77d782599f44f75606c2437ccd19783692354a52
ast-wwi <armin.steinkasserer@ww-informatik.de>
2025-07-15 10:22:23 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+_ww_load_js(
+    [
+        _ww_app_baseUrl + "html.js",
+        _ww_app_baseUrl + "tabulator.js",
+        _ww_app_baseUrl + "bootstrap5.bundle.min.js",
+        _ww_app_baseUrl + "luxon.js",
+        _ww_app_baseUrl + "main.js",
+    ]
+);
